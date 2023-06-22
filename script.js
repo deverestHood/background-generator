675bd46be2e50704423871770a6b1116a61b3e12
Everest Hood <everesthood@gmail.com>
2023-06-22 14:03:42 -0400

    Add ES6 syntax and style changes

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var randomButton = document.getElementById("random");
+const css = document.querySelector('.language-css');
+const [color1, color2] = document.querySelectorAll('.color')
+const body = document.getElementById('gradient');
 
-function setGradient() {
-	body.style.background = "linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+const randomButton = document.getElementById('random');
 
-	css.textContent = body.style.background + ";";
+setGradient = () => {
+	body.style.background = _linearGradient();
+	css.textContent = '${body.style.background};';
+}
+
+_linearGradient = () => {
+	return 'linear-gradient(to right, ${color1.value}, ${color2.value})';
+}
+
+navigator.permissions.query({ name: "write-on-clipboard" }).then((result) => {
+	if (result.state == "granted" || result.state == "prompt") {
+	  alert("Write access granted!");
+	}
+  });
+
+let copy = document.getElementById('copy-css').innerHTML;
+
+const copyContent = async () => {
+	try {
+	await navigator.clipboard.writeText(copy);
+	console.log('Content copied to clipboard');
+	} catch (err) {
+	console.error('Failed to copy: ', err);
+	}
 }
 
 // .addEventListener("load", setGradient);
 
-function getRandomColor() {
-	var letters = "0123456789ABCDEF";
-	var color = "#";
-	for (var i = 0; i < 6; i++) {
+getRandomColor = () => {
+	let letters = "0123456789ABCDEF";
+	let color = "#";
+	for (let i = 0; i < 6; i++) {
 		color+= letters[Math.floor(Math.random() * 16)];
 	}
 	return color;
 }
 
-function randomColors() {
-	color1.value = getRandomColor();
-	color2.value = getRandomColor();
+randomColors = () => {
+	color1.value, color2.value = getRandomColor();
 
 	setGradient();
 }
 
 setGradient();
 
-color1.addEventListener("input", setGradient);
+color1.addEventListener('input', setGradient);
 
-color2.addEventListener("input", setGradient);
+color2.addEventListener('input', setGradient);
 
-randomButton.addEventListener("click", randomColors);
+randomButton.addEventListener('click', randomColors);
 
 
 
